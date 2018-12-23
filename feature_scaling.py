84297304e55c2fd1ab4f4fa4c2cb8978c9291295
Akshay Parkar <33171910+parkar10@users.noreply.github.com>
2018-12-23 19:12:13 +0530

    Update feature_scaling.py

--- feature_scaling.py
+++ feature_scaling.py
@@ -9,7 +9,8 @@ class FeatureScaling:
         output_rawCSV = rawCSV['price']
         input_rawCSV = rawCSV.drop(['id','date','price'], axis = 1)
         scaler = MinMaxScaler()
-        input_rawCSV[['sqft_living' , 'sqft_lot' , 'sqft_living15' , 'sqft_lot15']] = scaler.fit_transform(input_rawCSV[['sqft_living' , 'sqft_lot' , 'sqft_living15' , 'sqft_lot15']])
+        scalar..partial_fit(input_rawCSV)
+        input_rawCSV[['sqft_living' , 'sqft_lot' , 'sqft_living15' , 'sqft_lot15']] = scaler.ransform(input_rawCSV[['sqft_living' , 'sqft_lot' , 'sqft_living15' , 'sqft_lot15']])
         return input_rawCSV,output_rawCSV
 
 
